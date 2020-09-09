7d9fe83ac4b8c5d0a4628a9df77ce897638389e3
Marciel Felipe <42660706+marcielfelipe@users.noreply.github.com>
2020-09-08 22:13:03 -0300

    title

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+import './global.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">JackPot</h1>
+    </div>
+  );
+}
+
+export default App;
